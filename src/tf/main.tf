51cd1e46dd33c749100df3e09fb00cc600dff1b8
PrateekPrakash008 <118456658+PrateekPrakash008@users.noreply.github.com>
2023-01-02 15:14:49 +0530

    Create main.tf

---
+++ src/tf/main.tf
@@ -0,0 +1,49 @@
+#--Service Provider AMA Definition--
+data "azurerm_role_definition" "builtin" {
+  name = "Contributor"
+}
+data "azurerm_client_config" "current" {}
+
+
+resource "azurerm_resource_group" "rg-service-provider" {
+  name     = "rg-${var.proj_code}-definition-471"
+  location = var.location
+}
+
+
+resource "azurerm_managed_application_definition" "ama-def" {
+  name                = "managedapplication471"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg-service-provider.name
+  lock_level          = "ReadOnly"
+  package_file_uri    = "https://github.com/PrateekPrakash008/ama_test/blob/main/AMA.zip"
+  display_name        = "Testmanagedapplication471"
+  description         = "ForTheHybridModel"
+
+  authorization {
+    service_principal_id = data.azurerm_client_config.current.object_id
+    role_definition_id   = split("/", data.azurerm_role_definition.builtin.id)[length(split("/", data.azurerm_role_definition.builtin.id)) - 1]
+  }
+
+  authorization {
+    service_principal_id = var.Security_Group_Id
+    role_definition_id   = split("/", data.azurerm_role_definition.builtin.id)[length(split("/", data.azurerm_role_definition.builtin.id)) - 1]
+  }
+}
+#--Service Provider AMA--
+resource "azurerm_managed_application" "ama01" {
+  name                        = "${var.proj_code}-ama-service-provider-471"
+  location                    = var.location
+  resource_group_name         = azurerm_resource_group.rg-service-provider.name
+  kind                        = "ServiceCatalog"
+  managed_resource_group_name = "rg-${var.proj_code}-ama-customer-471"
+  application_definition_id   = azurerm_managed_application_definition.ama-def.id
+
+  parameters = {
+    location                 = var.location
+    storageAccountNamePrefix = "01"
+    storageAccountType       = "Standard_LRS"
+  }
+}
+
+data "azurerm_subscription" "example" {}
