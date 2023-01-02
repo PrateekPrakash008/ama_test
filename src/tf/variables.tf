986fa99757fd2cd815ad303bf4da2dd1b367d340
PrateekPrakash008 <118456658+PrateekPrakash008@users.noreply.github.com>
2023-01-02 15:15:15 +0530

    Create variables.tf

---
+++ src/tf/variables.tf
@@ -0,0 +1,12 @@
+variable "proj_code" {
+  description = "Project code"
+  default     = "hybrid"
+}
+
+variable "location" {
+  default = "Central India"
+}
+
+variable "Security_Group_Id"{
+  default = "dcd7864a-ac76-44ec-b980-862343c0353b"
+}
